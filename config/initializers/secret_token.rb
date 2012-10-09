a17006ec0ed2334645dcc1f9d8a85b31f8f60e04
brntbeer <brent.beer@gmail.com>
2012-10-09 09:39:46 -0700

    a wild Rails appeared! :memo:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViralDispatch::Application.config.secret_token = 'f84a6f60591211f85fca2af4c27581503d3e21ac72a44d2141869b4a5ef5fd75f3b76e4fe76baf1c0700ed2e4b12c76b479e597f13b0beaacdc424898a5dc4a3'
